88af385d67ff64a21ea65529aa22d43151f28c75
Guilherme Oliveira <oliveiraguilherme@ufg.br>
2022-12-25 15:57:42 -0300

    tudoéobjeto

---
+++ javascript/funcoes/script.js
@@ -0,0 +1,15 @@
+function areaQuadrado(lado) {
+  return lado * lado;
+}
+console.log(areaQuadrado(2));
+
+function pi() {
+  return 3.14;
+}
+
+var total = 3 * pi();
+console.log(total);
+
+addEventListener('click', function () {
+  console.log('Clicou');
+});
---
+++ javascript/objetos/script.js
@@ -0,0 +1,4 @@
+var cachorro = {
+    nome: "Rex",
+    
+}
